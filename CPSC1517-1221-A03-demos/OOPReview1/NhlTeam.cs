f51f441c75bfb04c063d30d43093803d4bda0799
Bon Levi Calderon <bcalderon2@nait.ca>
2022-09-07 16:55:03 -0600

    HockeyTeamSenator

---
+++ CPSC1517-1221-A03-demos/OOPReview1/NhlTeam.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPReview1
+{
+
+    public class NhlTeam
+    {
+        public NhlConference Conference { get; private set; }
+        public NhlDivision Division { get; private set; }
+        private string _name; //field for the Name property
+        public string Name 
+        { 
+            get => _name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Name must contain text.");
+                }
+                _name = value.Trim();
+            }
+        }
+        private string _city;
+        public string City
+        {
+            get => _city;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("City must contain text.");
+                }
+                _city = value.Trim();
+            }
+        }
+        private int _gamesPlayed;
+        public int GamesPlayed 
+        { 
+            get => _gamesPlayed;
+            set
+            {
+                if( value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Games played has to be greater than 0.");
+                }
+                _gamesPlayed = value;
+            }
+        }
+        private int _wins;
+        public int Wins
+        {
+            get => _wins;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Wins has to be greater than or equal to 0.");
+                }
+                _wins = value;
+            }
+        }
+        private int _losses;
+        public int Losses
+        {
+            get => _losses;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Losses has to be greater than or equal to 0.");
+                }
+                _wins = value;
+            }
+        }
+        private int _overtimeLosses;
+        public int OvertimeLosses
+        {
+            get => _overtimeLosses;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Overtime Losses has to be greater than or equal 0.");
+                }
+                _overtimeLosses = value;
+            }
+        }
+        public int Points => (Wins * 2) + OvertimeLosses;
+        
+        public NhlTeam(NhlConference conference, NhlDivision division, string name,string city)
+        {
+            Conference = conference;
+            Division = division;
+            Name = name;
+            this.City = city;
+
+            GamesPlayed = 0;
+            Wins = 0;
+            Losses = 0;
+            OvertimeLosses = 0;
+        }
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            return $"{Conference},{Division},{Name},{City},{GamesPlayed},{Wins},{Losses},{OvertimeLosses}";
+        }
+
+
+    }
+}
--- CPSC1517-1221-A03-demos/OOPReview1/Program.cs
+++ CPSC1517-1221-A03-demos/OOPReview1/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using OOPReview1;
+
+NhlTeam Senators = new NhlTeam(NhlConference.Easter,NhlDivision.Atlantic,"Senators","Ottawa");
+Senators.GamesPlayed = 82;
+Senators.Wins = 33;
+Senators.Losses = 42;
+Senators.OvertimeLosses = 7;
+//pring the points should be 73
+Console.Write(Senators);
+Console.WriteLine($"\nPoints = {Senators.Points}");
