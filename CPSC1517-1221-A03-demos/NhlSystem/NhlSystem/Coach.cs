fa923903e04b896fed95c0161482d63ab8260a67
Levi Calderon <bcalderon2@nait.ca>
2022-09-16 11:46:33 -0600

    09/16/2022 end of class

--- CPSC1517-1221-A03-demos/NhlSystem/NhlSystem/Coach.cs
+++ CPSC1517-1221-A03-demos/NhlSystem/NhlSystem/Coach.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace NhlSystem
 {
-    public class Coach
+    //define class name Coach that inherits from the base class Person
+    public class Coach : Person // colon is the inheritance  meanas Coach class will inherit from Person class
     {
+        //Define an auto-implemented property for HireDate
+        public DateTime HireDate { get; set; }
+
+        //define cosntructor that passes FullName to the Person base Class
+        public Coach(String fullName, DateTime hireDate) : base(fullName)
+        {
+            HireDate = hireDate;
+        }
+
     }
 }
--- CPSC1517-1221-A03-demos/NhlSystem/NhlSystem/Player.cs
+++ CPSC1517-1221-A03-demos/NhlSystem/NhlSystem/Player.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace NhlSystem
 {
-    public class Player
+    //define class name player that inherits from person
+    public class Player : Person
     {
+        //define auto-implemented properties for prosition,goals,assists,and primary number
+        public int PrimaryNo { get; set; } 
+        public NhlPosition Position { get; set; }
+        public int Goals { get; set; }  
+        public int Assists { get; set; }    
+
+        //define a read-only computed property for points
+        public int Points
+        {
+            get { return Goals + Assists; }
+        }
+        //define constructor that passes fullName to Person base class
+        public Player(string fullName, NhlPosition position, int primaryNo) : base(fullName)
+        {
+            Position = position;
+            PrimaryNo = primaryNo;
+            Goals = 0;
+            Assists = 0;
+        }
+
     }
 }
