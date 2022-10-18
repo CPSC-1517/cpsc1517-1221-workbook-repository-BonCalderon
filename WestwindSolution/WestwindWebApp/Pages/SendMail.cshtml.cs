0baef2d2478625504e8fc0db9fe0bd97ef10bc1b
bcalderon2 <bcalderon2@nait.ca>
2022-10-18 14:29:41 -0600

    near end of class finished email sending

--- WestwindSolution/WestwindWebApp/Pages/SendMail.cshtml.cs
+++ WestwindSolution/WestwindWebApp/Pages/SendMail.cshtml.cs
@@ -8,6 +8,21 @@ namespace WestwindWebApp.Pages
 {
     public class SendMailModel : PageModel
     {
+        public void OnGet() 
+        {
+            //introduction instruction on dependancy injection
+            //var gmailUsername = Configuration["GmailCredentials:Username"];  
+            //var gmailAppPassword = Configuration["GmailCredentials:Password"];
+            //FeedBackMessage = $"Gmail username = {gmailUsername} <br />";
+            //FeedBackMessage += $"Gmail app password = {gmailAppPassword} <br />";
+        }
+
+        private readonly IConfiguration Configuration;
+        public SendMailModel(IConfiguration configuration) //constructor dependency injection getting the configuration for the setting (configuration)
+        {
+            this.Configuration = configuration;  //relates to json file appsetting
+        }
+
         [BindProperty]
         public string FeedBackMessage { get; set; }
 
@@ -26,24 +41,47 @@ namespace WestwindWebApp.Pages
         [BindProperty]
         public string MailMessage { get; set; }
 
-
-        public void OnGet()
+        //page handler methods
+        public void OnPostSendMail()
         {
+            //FeedBackMessage = "<h2>Send Mail button clicked</h2>";
 
-            //var sendMailClient = new SmtpClient();
-            //sendMailClient.Host = "smtp@gmail.com";
-            //sendMailClient.Port = 587;
-            //sendMailClient.EnableSsl = true;
-            //var sendMailCredentials = new NetworkCredential();
-            //sendMailCredentials.UserName = Username;
-            //sendMailCredentials.Password = Password;
-            //sendMailClient.Credentials = sendMailCredentials;
+            var sendMailClient = new SmtpClient(); //smtp protocol that allows you to send email
+            sendMailClient.Host = "smtp.gmail.com"; //to send email specify which server to send it
+            sendMailClient.Port = 587; //need to set what port to yuse
+            sendMailClient.EnableSsl = true; 
+            var sendMailCredentials = new NetworkCredential(); // identity of the user by specifying their user name and password
+            Username = Configuration["GmailCredentials:Username"];
+            Password = Configuration["GmailCredentials:Password"];
+            sendMailCredentials.UserName = Username;
+            sendMailCredentials.Password = Password;
+            sendMailClient.Credentials = sendMailCredentials;
 
-            //var mailMessage = new MailMessage(Username, Recipient);
-            //mailMessage.Subject = MailSubject;
-            //mailMessage.Body = MailMessage;
-            //sendMailClient.Send(mailMessage);
+            var sendFromAddress = new MailAddress(Username);
+            var sendToAddress = new MailAddress(Recipient);
+
+            var mailMessage = new MailMessage(sendFromAddress, sendToAddress);
+            mailMessage.Subject = MailSubject;
+            mailMessage.Body = MailMessage;
+
+            try
+            {
+                sendMailClient.Send(mailMessage);
+                FeedBackMessage = "<div class='alert alert-primary'>Email Sent!</div>";
+            }
+            catch (Exception ex)
+            {
+                FeedBackMessage = $"<div class='alert alert-danger'>Error sending email {ex}!</div>";
+
+            }
+            
         }
+
+        public void OnPostClearForm()
+        {
+            FeedBackMessage = "<h2>Clear Form button clicked</h2>";
+        }
+
         public void OnPost()
         {
 
