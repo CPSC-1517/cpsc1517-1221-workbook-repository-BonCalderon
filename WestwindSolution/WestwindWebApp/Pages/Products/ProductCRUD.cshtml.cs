fe1a241a8ab450dfb58b1eeb630067fc77338611
bcalderon2 <bcalderon2@nait.ca>
2022-11-23 16:50:00 -0700

    End of Class CRUD

--- WestwindSolution/WestwindWebApp/Pages/Products/ProductCRUD.cshtml.cs
+++ WestwindSolution/WestwindWebApp/Pages/Products/ProductCRUD.cshtml.cs
@@ -1,10 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WestWindSystem.BLL;
 
 namespace WestwindWebApp.Pages.Products
 {
     public class ProductCRUDModel : PageModel
     {
+        #region internal fields
+        private readonly CategoryServices _categoryServices;
+        private readonly ProductServices _productServices;
+        #endregion
+
+        #region data properties for page
+        [BindProperty(SupportsGet = true)]
+        public int? EditProductId { get; set; }  
+
+        #endregion
+
+
+
         public void OnGet()
         {
         }
