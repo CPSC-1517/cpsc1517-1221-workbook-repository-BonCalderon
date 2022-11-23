5d3288a9eceea8294d5ffb35d3c047bf7d5f4ee2
bcalderon2 <bcalderon2@nait.ca>
2022-11-23 16:45:59 -0700

    CRUD product

--- WestwindSolution/WestwindWebApp/Pages/Products/ProductCategory.cshtml.cs
+++ WestwindSolution/WestwindWebApp/Pages/Products/ProductCategory.cshtml.cs
@@ -21,54 +21,75 @@ namespace WestwindWebApp.Pages.Products
             _productServices = productServices;
 
             // Fetch from the system (CategoryServices) a list of Category
-            CategoryList = _categoryServices.List();
-            //CategorySelectionList = new SelectList(_categoryServices.List(), "Id", "CategoryName", SelectedCategoryId); //populate dropdown selection menu
+            List<Category> categories = _categoryServices.List();
+            CategorySelectList = new SelectList(categories, "Id", "CategoryName", SelectedCategoryId); //populate dropdown selection menu
 
         }
         #endregion
 
         #region Properties to populate Category select element and track is selected value
-        public List<Category> CategoryList { get; private set; }
-        [BindProperty]
-        public int? SelectedCategoryId { get; set; }
+        //public List<Category> CategoryList { get; private set; }
 
-        //public SelectList CategorySelectionList { get; private set; }
+        //Data source for category select element
+        public SelectList CategorySelectList { get; private set; }
+        
+        [BindProperty] //bindable property for value selected from select element 
+        public int? SelectedCategoryId { get; set; }
+        
+        //query result list propery
+        public List<Product>? QueryProductResultList { get; private set; }
         #endregion
 
-        [BindProperty]
-        public string? QueryProductName { get; set; }
+        //[BindProperty]
+        //public string? QueryProductName { get; set; }
 
-        [TempData]
-        public string? FeedbackMessage { get; set; }
+        //feedback message property
+        
+        public string? InfoMessage { get; set; }
+        public string? ErrorMessage { get; set; }
 
-        public List<Product> QueryResultList { get; private set; }
+       
 
         public void OnGet()
         {
 
         }
 
-        public void OnPostSearchByCategory()
+        public IActionResult OnPostFetchProducts()
         {
-            FeedbackMessage = "You click on Search By Category";
-            QueryResultList = _productServices.FindProductsByCategoryId(SelectedCategoryId.Value);
-            //return RedirectToPage(new { currentSelectedCategoryId = SelectedCategoryId });
+            IActionResult nextPage = Page();
+            // verify that a valid category was selected
+            if (SelectedCategoryId.HasValue && SelectedCategoryId.Value > 0)
+            {
+                int categoryId = SelectedCategoryId.Value;
+                QueryProductResultList = _productServices.FindProductsByCategoryId(categoryId);
+                InfoMessage = $"Query Returned {QueryProductResultList.Count} record(s).";
+            }
+            else
+            {
+                ErrorMessage = "A valid category must be selected";
+            }
+          
+            return nextPage;
         }
 
-        public void OnPostSearchByProductName()
-        {
-            FeedbackMessage = "You click on Search By Product Name";
-            QueryResultList = _productServices.FindProductsByProductName(QueryProductName);
 
-            //return RedirectToPage();
+        public IActionResult OnPostClear()
+        {
+            IActionResult nextPage = Page();
+            SelectedCategoryId = null;
+            QueryProductResultList = null;
+            ModelState.Clear();
+
+
+            return nextPage;
         }
 
-        public IActionResult OnPostClearForm()
+        public IActionResult OnPostNewProduct()
         {
-            FeedbackMessage = "You click on Clear button";
-            //SelectedCategoryId = 0;
-            //QueryProductName = null;
-            return RedirectToPage();
+            IActionResult nextPage = RedirectToPage("/Products/ProductCRUD"); // inside the redirect page is the route you want it to redirect to. Razor pages access a route not a file name , do not inlcude file extension
+
+            return nextPage;
         }
    
     }
